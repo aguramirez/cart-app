d1c69be1d536e9b0a2627fe95b25602153a6b180
aguramirez <agustinramirez58@gmail.com>
2023-07-17 21:37:18 -0300

    componentes

--- src/components/ProductCardView.jsx
+++ src/components/ProductCardView.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export const ProductCardView = ({id, name, description, price}) => {
 
-    const onAddProduct = () => {
-
+    const onAddProduct = (product) => {
+        console.log(product);
     } 
     return (
         <>
