546f851acb1550c35ef4ad349d573057d7c9162d
aguramirez <agustinramirez58@gmail.com>
2023-08-09 18:58:13 -0300

    conectando con el backend y agrego isLoading

--- src/components/CatalogView.jsx
+++ src/components/CatalogView.jsx
@@ -6,19 +6,35 @@ export const CatalogView = ({handler}) => {
 
     const [products, setProducts] = useState([]);
 
+    const [isLoading, setIsLoading] = useState(true);
+
+    const findAll = async() => {
+        const productosBackend = await getProducts();
+        setProducts(productosBackend);
+        setIsLoading(false);
+    }
+
     useEffect(
         () => {
-            setProducts(getProducts());
-
+            findAll();
         }, []
     )
 
     return (
         <>
+            {
+                isLoading && 
+                <div className="alert alert-info">Cargando ...</div>
+            }
             <div className="row">
                 {products.map(p => (
                     <div className="col-4 my-2" key={p.id}>
-                        <ProductCardView handler={handler} id={p.id} name={p.name} description={p.description} price={p.price} />
+                        <ProductCardView 
+                        handler={handler} 
+                        id={p.id} 
+                        name={p.name} 
+                        description={p.description} 
+                        price={p.price} />
                     </div>
                 ))}
             </div>
